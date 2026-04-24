4615e850d8957b020a587b1ad561d45ce6692e18
nunomcruz <github@crz.pt>
2026-04-24 11:04:50 +0100

    release: update flash command with additional parameters for mode, size, and frequency

--- scripts/release.sh
+++ scripts/release.sh
@@ -83,6 +83,7 @@ Flash from browser: https://nunomcruz.github.io/micropython-lorawan/
 Or manually with esptool:
 \`\`\`
 esptool.py --chip esp32 --baud 460800 write_flash \\
+    --flash_mode dio --flash_size 4MB --flash_freq 80m \\
     0x1000 bootloader.bin \\
     0x8000 partition-table.bin \\
     0x10000 micropython.bin
